c4793458abc26f1b2c79308f2da83ea252736ec1
CHOUIKA Yassir <yassir.chouika@gmail.com>
2025-05-19 19:41:16 +0200

    feat: add syntax highlighting to code snippets

--- src/renderer/src/components/SnippetCard.jsx
+++ src/renderer/src/components/SnippetCard.jsx
@@ -1,5 +1,7 @@
 import PropTypes from 'prop-types'
 import { useNavigate } from 'react-router-dom'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { oneLight } from 'react-syntax-highlighter/dist/esm/styles/prism'
 import '../assets/SnippetCard.css'
 
 const SnippetCard = ({ snippet, onView, onDelete }) => {
@@ -8,16 +10,24 @@ const SnippetCard = ({ snippet, onView, onDelete }) => {
   return (
     <div className="snippet-card">
       <div className="snippet-header">
-        <h3 style={{ cursor: 'pointer' }} onClick={() => navigate(`/fragment/${snippet.id}`)}>
-          {snippet.title}
-        </h3>
+        <h3 onClick={() => navigate(`/fragment/${snippet.id}`)}>{snippet.title}</h3>
 
         <div className="snippet-actions">
           <button onClick={() => onView(snippet)}>View</button>
           <button onClick={() => onDelete(snippet.id)}>Delete</button>
         </div>
       </div>
-      <pre className="snippet-code">{snippet.code}</pre>
+
+      <SyntaxHighlighter
+        language={snippet.language || 'javascript'}
+        style={oneLight}
+        className="snippet-code"
+        showLineNumbers={false}
+        wrapLines={true}
+      >
+        {snippet.code}
+      </SyntaxHighlighter>
+
       <div className="snippet-tags">
         {snippet.tags.map((tag) => (
           <span key={tag} className="tag">
@@ -29,13 +39,13 @@ const SnippetCard = ({ snippet, onView, onDelete }) => {
   )
 }
 
-// this block to validate props and suppress ESLint
 SnippetCard.propTypes = {
   snippet: PropTypes.shape({
     id: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     code: PropTypes.string.isRequired,
-    tags: PropTypes.arrayOf(PropTypes.string).isRequired
+    tags: PropTypes.arrayOf(PropTypes.string).isRequired,
+    language: PropTypes.string
   }).isRequired,
   onView: PropTypes.func.isRequired,
   onDelete: PropTypes.func.isRequired
